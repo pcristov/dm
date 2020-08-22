d3b2d5bdb5291c6c3de26d5d66594f5a90ab5c74
Pedro Cristovão <pcristov@gmail.com>
2020-08-22 22:42:12 +0800

    Add id attribute to drum-pad elements

--- src/DrumMachine.js
+++ src/DrumMachine.js
@@ -30,44 +30,48 @@ render() {
 	return (
 		<div id="drum-machine">
 			<div id="display">
+			Display
+			</div>
+			
+			<div id="grid-wrapper">
 				<div class="grid">
-					<div class="drum-pad">
+					<div id="Heater-1" class="drum-pad" onclick={}>
 						Q
 						<audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3" id="Q" />
 					</div>
-					<div class="drum-pad">
+					<div id="Heater-2" class="drum-pad" onclick={}>
 						W
 						<audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3" id="W" />
 					</div>
-					<div class="drum-pad">
+					<div id="Heater-3" class="drum-pad" onclick={}>
 						E
 						<audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3" id="E" />
 					</div>
 					
 					
-					<div class="drum-pad">
+					<div id="Heater-4" class="drum-pad" onclick={}>
 						A
 						<audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3" id="A" />
 					</div>
-					<div class="drum-pad">
+					<div id="Clap" class="drum-pad" onclick={}>
 						S
 						<audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3" id="S" />
 					</div>
-					<div class="drum-pad">
+					<div id="Open-HH" class="drum-pad" onclick={}>
 						D
 						<audio src="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3" id="D" />
 					</div>
 					
 					
-					<div class="drum-pad">
+					<div id="Kick-n'-Hat" class="drum-pad" onclick={}>
 						Z
 						<audio src="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3" id="Z" />
 					</div>
-					<div class="drum-pad">
+					<div id="Kick" class="drum-pad" onclick={}>
 						X
 						<audio src="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3" id="X" />
 					</div>
-					<div class="drum-pad">
+					<div id="Closed-HH" class="drum-pad" onclick={}>
 						C
 						<audio src="https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3" id="C" />
 					</div>
